31b760e83f3231ee88e418ff946118973234e126
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2024-12-10 13:44:23 +0530

    Time: 18 ms (83.04%) | Memory: 58.2 MB (25.95%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,20 +1,31 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+        //I  got Runtime: 18 ms, faster than 83.04% of Java online submissions for Contains Duplicate II. So I am trying it below again.
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.containsKey(nums[i])){
+        //         int index=map.get(nums[i]);  
+        //         //returns value at which nums[i] is present i.e. returns index which with it was saved.
+        //         if(Math.abs(index-i)<=k){
+        //             return true;
+        //         }
+        //         else{
+        //             map.put(nums[i],i);
+        //         }
+        //     }
+        //     else{
+        //         map.put(nums[i],i);
+        //     }
+        // }
+        // return false;
+        
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                int index=map.get(nums[i]);  
-                //returns value at which nums[i] is present i.e. returns index which with it was saved.
-                if(Math.abs(index-i)<=k){
-                    return true;
-                }
-                else{
-                    map.put(nums[i],i);
-                }
+            if(map.containsKey(nums[i]) && Math.abs(map.get(nums[i])-i)<=k){
+                return true;
             }
-            else{
-                map.put(nums[i],i);
-            }
+            map.put(nums[i],i);
         }
         return false;
     }
