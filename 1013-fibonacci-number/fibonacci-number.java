921233ab07fa4ee2485f866b4ed0f7033e5c99ea
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-02-14 18:53:11 +0530

    Time: 8 ms (47.47%) | Memory: 40.4 MB (62.68%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int n) {
+        // int n1=0;
+        // int n2=1;
+        // int n3=0;
+        // while(n!=0){
+        //     n3=n2+n1;
+        //     n1=n2;
+        //     n2=n3;
+        //     n3=n1;
+        //     n--;
+        // }
+        // return n3;
+
+
+        // I am going to try this using recurssion now:
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
