7fa3b2f30c19c15d9f8d4d92844e809b528710a0
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-02-14 18:54:11 +0530

    Time: 0 ms (100%), Space: 40.8 MB (17.82%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,22 +1,23 @@
 class Solution {
     public int fib(int n) {
-        // int n1=0;
-        // int n2=1;
-        // int n3=0;
-        // while(n!=0){
-        //     n3=n2+n1;
-        //     n1=n2;
-        //     n2=n3;
-        //     n3=n1;
-        //     n--;
-        // }
-        // return n3;
+        int n1=0;
+        int n2=1;
+        int n3=0;
+        while(n!=0){
+            n3=n2+n1;
+            n1=n2;
+            n2=n3;
+            n3=n1;
+            n--;
+        }
+        return n3;
 
 
         // I am going to try this using recurssion now:
-        if(n<=1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        // if(n<=1){
+        //     return n;
+        // }
+        // return fib(n-1)+fib(n-2);
+        // code runs but takes more time of around 8ms which means recurssion has higher time complexity in this case.
     }
 }
