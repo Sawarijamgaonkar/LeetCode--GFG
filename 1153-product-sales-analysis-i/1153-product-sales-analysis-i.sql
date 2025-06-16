63962ae541d42e920712d275a82819103e8f991a
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-06-16 10:25:10 +0530

    Time: 1157 ms (87.18%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select product_name, year, price from Product inner join Sales using(product_id);
+-- time complexity 1315ms , beats 37.91%:
+-- select product_name, year, price from Product inner join Sales using(product_id);
+-- better approach:
+select product_name, year,price from Product inner join Sales on Sales.product_id=Product.product_id;
