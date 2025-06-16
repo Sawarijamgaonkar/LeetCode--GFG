8bacbf1cb89028c04e21e19230d027b5e43fcaf2
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-06-16 10:28:27 +0530

    Time: 1447 ms (17.29%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -2,8 +2,12 @@
 -- time complexity 1315ms , beats 37.91%:
 -- select product_name, year, price from Product inner join Sales using(product_id);
 
--- better approach with time complexity 1157ms, beats 87.18% :
+-- better approach: 1220ms , beats 65.18%
+-- select product_name, year,price from Sales inner join Product on Sales.product_id=Product.product_id;
+
+-- more better approach with time complexity 1157ms, beats 87.18% :
 -- select product_name, year,price from Product inner join Sales on Sales.product_id=Product.product_id;
 
--- more better approach:
-select product_name, year,price from Sales inner join Product on Sales.product_id=Product.product_id;
+-- best approach:
+select Product.product_name, Sales.year,Sales.price from Sales inner join Product on Sales.product_id=Product.product_id;
+
