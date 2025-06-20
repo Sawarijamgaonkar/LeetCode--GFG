8264c08d3f0be1b9bc8a559b42b9f21e1ebf194c
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-06-20 10:54:22 +0530

    Time: 0 ms (100%), Space: 42.2 MB (11.69%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,17 +14,33 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        return isMirror(root.left,root.right);
+    // public boolean isSymmetric(TreeNode root) {
+    //     return isMirror(root.left,root.right);
 
-    }
-    public boolean isMirror(TreeNode n1, TreeNode n2){
-        if(n1==null && n2==null){
+    // }
+    // public boolean isMirror(TreeNode n1, TreeNode n2){
+    //     if(n1==null && n2==null){
+    //         return true;
+    //     }
+    //     if(n1==null || n2==null){
+    //         return false;
+    //     }
+    //     return n1.val==n2.val && isMirror(n1.left,n2.right) && isMirror(n1.right,n2.left);
+    // }
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null){
             return true;
         }
-        if(n1==null || n2==null){
+        return isMirror(root.left,root.right);
+    }
+    public boolean isMirror(TreeNode x, TreeNode y){
+        if(x==null && y==null){
+            return true;
+        }
+        if(x==null || y==null){
             return false;
         }
-        return n1.val==n2.val && isMirror(n1.left,n2.right) && isMirror(n1.right,n2.left);
+        return x.val==y.val && isMirror(x.left, y.right) && isMirror(x.right,y.left);
     }
+
 }
