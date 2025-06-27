85c512364e407cd6537108a5917413138c10f385
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-06-27 13:52:54 +0530

    Time: 2 ms (54.87%), Space: 46.1 MB (73.47%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,15 +1,50 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] arr=new int[nums1.length+nums2.length];
-        System.arraycopy(nums1,0,arr,0, nums1.length);
-        System.arraycopy(nums2,0,arr,nums1.length,nums2.length);
-        Arrays.sort(arr);
+    // public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    //     int[] arr=new int[nums1.length+nums2.length];
+    //     System.arraycopy(nums1,0,arr,0, nums1.length);
+    //     System.arraycopy(nums2,0,arr,nums1.length,nums2.length);
+    //     Arrays.sort(arr);
+    //     if(arr.length%2==0){
+    //         return (double)(arr[arr.length/2]+arr[(arr.length/2)-1])/2;
+    //     }
+    //     else{
+    //         return (double)arr[arr.length/2];
+    //     }
+    //     // return (double)0;
+    // }
+    public double findMedianSortedArrays(int[] nums1,int[]nums2){
+        int arr[]=new int[nums1.length+nums2.length];
+        int i=0, j=0,k=0;
+        int median1=0;
+        int median2=0;
+        while(i<nums1.length&& j<nums2.length){
+            if(nums1[i]>nums2[j]){
+                arr[k]=nums2[j];
+                j++;
+                k++;
+            }
+            else if(nums1[i]<=nums2[j]){
+                arr[k]=nums1[i];
+                i++;
+                k++;
+            }
+        }
+        while(i<nums1.length){
+            arr[k]=nums1[i];
+            i++;
+            k++;
+        }
+        while(j<nums2.length){
+            arr[k]=nums2[j];
+            j++;
+            k++;
+        }
         if(arr.length%2==0){
             return (double)(arr[arr.length/2]+arr[(arr.length/2)-1])/2;
         }
         else{
             return (double)arr[arr.length/2];
         }
-        // return (double)0;
+        
     }
 }
