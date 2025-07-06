feddaaf5f89566aaf843f58e7c89a6b2c66ec83a
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-07-06 11:56:08 +0530

    Time: 61 ms (54.37%), Space: 61.3 MB (40.92%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -34,8 +34,19 @@ class Solution {
     //     }
     // }
 
+    // 23ms and beats 83.69%: 
+    // public int findKthLargest(int[] nums,int k) {
+    //     Arrays.sort(nums);
+    //     return nums[nums.length-k];
+    // }
     public int findKthLargest(int[] nums,int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            minHeap.add(nums[i]);
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
     }
 }
