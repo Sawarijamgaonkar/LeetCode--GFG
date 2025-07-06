8b15c51cf9f209a5c3b4edc4ba3761de3826bb20
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-07-06 11:51:07 +0530

    Time: 23 ms (83.69%), Space: 57.6 MB (71.9%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,36 +1,41 @@
 class Solution {
-    // heap sort: 38ms beats 32.27% : 
+    // heap sort: 805ms, beats 5%:  
+    // public int findKthLargest(int[] nums,int k) {
+    //     for(int i=(nums.length/2)-1;i>=0;i--){
+    //         heapify(nums,nums.length,i);
+    //     }
+    //     for(int i=nums.length-1;i>0;i--){
+    //         swap(nums,0,i);
+    //         heapify(nums,i,0);
+    //     }
+    //     for(int i=0;i<nums.length;i++){
+    //         System.out.println(nums[i]+" ");
+    //     }
+    //     return nums[nums.length-k];
+    // }
+    // public static void swap(int[] arr, int i, int j){
+    //     int temp=arr[i];
+    //     arr[i]=arr[j];
+    //     arr[j]=temp;
+    // }
+    // public static void heapify(int[] arr, int length,int index){
+    //     int max=index;
+    //     int left=2*index +1;
+    //     int right=2*index+2;
+    //     if(left<length && arr[left]>arr[max]){
+    //         max=left;
+    //     }
+    //     if(right<length && arr[right]>arr[max]){
+    //         max=right;
+    //     }
+    //     if(max!=index){
+    //         swap(arr,max,index);
+    //         heapify(arr,length,max);
+    //     }
+    // }
+
     public int findKthLargest(int[] nums,int k) {
-        for(int i=(nums.length/2)-1;i>=0;i--){
-            heapify(nums,nums.length,i);
-        }
-        for(int i=nums.length-1;i>0;i--){
-            swap(nums,0,i);
-            heapify(nums,i,0);
-        }
-        for(int i=0;i<nums.length;i++){
-            System.out.println(nums[i]+" ");
-        }
+        Arrays.sort(nums);
         return nums[nums.length-k];
-    }
-    public static void swap(int[] arr, int i, int j){
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-    }
-    public static void heapify(int[] arr, int length,int index){
-        int max=index;
-        int left=2*index +1;
-        int right=2*index+2;
-        if(left<length && arr[left]>arr[max]){
-            max=left;
-        }
-        if(right<length && arr[right]>arr[max]){
-            max=right;
-        }
-        if(max!=index){
-            swap(arr,max,index);
-            heapify(arr,length,max);
-        }
     }
 }
