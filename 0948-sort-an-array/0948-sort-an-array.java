262c7b1e138d6770054653c7540a8c8fc905779e
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-07-06 10:50:17 +0530

    Time: 38 ms (32.73%), Space: 55.8 MB (71.53%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,17 +1,33 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        // selection sort:
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=0;j<nums.length;j++){
-        //         if(nums[j]>nums[i]){
-        //             int temp=nums[i];
-        //             nums[i]=nums[j];
-        //             nums[j]=temp;
-        //         }
-        //     }
-        // }
-        // return nums;
-        Arrays.sort(nums);
+        for(int i=(nums.length/2)-1;i>=0;i--){
+            heapify(nums,nums.length,i);
+        }
+        for(int i=nums.length-1;i>0;i--){
+            swap(nums,0,i);
+            heapify(nums,i,0);
+        }
         return nums;
+    }
+    public static void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void heapify(int[] arr, int length,int index){
+        int max=index;
+        int left=2*index +1;
+        int right=2*index+2;
+        if(left<length && arr[left]>arr[max]){
+            max=left;
+        }
+        if(right<length && arr[right]>arr[max]){
+            max=right;
+        }
+        if(max!=index){
+            swap(arr,max,index);
+            heapify(arr,length,max);
+        }
+
     }
 }
