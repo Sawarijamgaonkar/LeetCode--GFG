2315af89a3d8c8e6cc75a307761fb6b0fe3005c8
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-07-07 16:17:56 +0530

    Time: 4 ms (100%), Space: 44.2 MB (48.42%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,12 +3,19 @@ class Solution {
         if(x<0){
             return false;
         }
-        String s=String.valueOf(x);
-        for(int i=0;i<s.length()/2;i++){
-            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
-                return false;
-            }
+        // String s=String.valueOf(x);
+        // for(int i=0;i<s.length()/2;i++){
+        //     if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        int rev=0;
+        int num=x;
+        while(num!=0){
+            rev=rev*10+num%10;
+            num=num/10;
         }
-        return true;
+        return (x==rev);
     }
 }
