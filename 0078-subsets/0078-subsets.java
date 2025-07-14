9e681f276b2abddff48f0e3d8fb67e7248c48f72
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-07-14 09:04:39 +0530

    Time: 0 ms (100%), Space: 43.1 MB (17.96%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,32 @@
+// class Solution {
+//     public List<List<Integer>> subsets(int[] nums) {
+//         List<List<Integer>> result=new ArrayList<>();
+//         Arrays.sort(nums);
+//         recurssion(result,new ArrayList<>(),nums,0);
+//         return result;
+//     }
+//     public void recurssion(List<List<Integer>> list, List<Integer> sublist, int[] nums, int start){
+//         list.add(new ArrayList<>(sublist));
+//         for(int i=start;i<nums.length;i++){
+//             sublist.add(nums[i]);
+//             recurssion(list,sublist,nums,i+1);
+//             sublist.remove(sublist.size()-1);
+//         }
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        Arrays.sort(nums);
-        recurssion(result,new ArrayList<>(),nums,0);
-        return result;
-    }
-    public void recurssion(List<List<Integer>> list, List<Integer> sublist, int[] nums, int start){
-        list.add(new ArrayList<>(sublist));
-        for(int i=start;i<nums.length;i++){
-            sublist.add(nums[i]);
-            recurssion(list,sublist,nums,i+1);
-            sublist.remove(sublist.size()-1);
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>()); // Add the empty subset
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(num);
+                result.add(subset);
+            }
         }
+        return result;
     }
 }
