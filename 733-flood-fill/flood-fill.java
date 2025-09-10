30059a627acebaa83bdc1a5e62aa77c11977f5d4
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-09-10 11:59:24 +0530

    Time: 1 ms (45.65%) | Memory: 45 MB (70.03%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,29 @@
+class Solution {
+    int[] dx=new int[]{0,1,0,-1};
+    int[] dy=new int[]{1,0,-1,0};
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        boolean[][] visited=new boolean[image.length][image[0].length];
+        int curr_color=image[sr][sc];
+        bfs(image,sr,sc,color,visited,curr_color);
+        return image;
+    }
+    public void bfs(int[][] image, int i, int j,int color,boolean[][] visited,int curr_color){
+        visited[i][j]=true;
+        if(image[i][j]==curr_color){
+            image[i][j]=color;
+            for(int k=0;k<4;k++){
+                int ni=i+dx[k];
+                int nj=j+dy[k];
+                if(isValid(ni,nj,image.length,image[0].length)){
+                    if(!visited[ni][nj]) bfs(image,ni,nj,color,visited,curr_color);
+                }
+            }
+        }
+    }
+    public boolean isValid(int i, int j,int n, int m){
+        if(i>n-1 || j>m-1 || i<0 || j<0){
+            return false;
+        }
+        return true;
+    }
+}
