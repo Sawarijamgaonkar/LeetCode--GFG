fccf11338af8bb07dfd4d5af93dad9eb8103a1c5
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-09-13 11:13:07 +0530

    Time: 6 ms (65.31%) | Memory: 45.6 MB (45.15%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        int vowelCount=0;
+        for(char ch:s.toCharArray()){
+            if(ch=='a'||ch=='i'||ch=='e'||ch=='o'||ch=='u') vowelCount++;
+        }
+        if(vowelCount==0) return false;
+        if(vowelCount%2!=0) return true;
+        return true;
+    }
+}
