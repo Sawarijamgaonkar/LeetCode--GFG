bf2d792448cd4af5723a2816abfd8a66d4620130
Sawari Jamgaonkar <122595291+Sawarijamgaonkar@users.noreply.github.com>
2025-09-13 11:14:28 +0530

    Time: 4 ms (80.36%), Space: 45.9 MB (8.42%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -2,10 +2,8 @@ class Solution {
     public boolean doesAliceWin(String s) {
         int vowelCount=0;
         for(char ch:s.toCharArray()){
-            if(ch=='a'||ch=='i'||ch=='e'||ch=='o'||ch=='u') vowelCount++;
+            if(ch=='a'||ch=='i'||ch=='e'||ch=='o'||ch=='u') return true;
         }
-        if(vowelCount==0) return false;
-        // if(vowelCount%2!=0) return true;
-        return true;
+        return false;
     }
 }
